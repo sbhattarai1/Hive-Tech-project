431d3113923a87071b54cbec797ac6f8a769c7bf
sbhattarai1 <sangita.bhattarai47@gmail.com>
2022-02-17 23:30:59 -0600

    add app

--- backend/apps/order_items/models.py
+++ backend/apps/order_items/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from apps.products.models import Product
+from apps.orders.models import order
 # Create your models here.
+from django.utils import timezone
+
+class OrderItem(models.Model):
+    class Meta(object):
+        db_table = 'order_item'
+
+    order = models.ForeignKey(
+        Order, related_name='related_order', on_delete=CASCADE
+    )
+    product = models.ForeignKey(
+        product, related_name='related_order_item_product', on _delete=models.CASCADE
+        )
+    qty = models.IntegerField(
+        'Quantity', blank=False, null=False
+        )
+
+    created_at = models.DateTimeField(
+        'Creation Date', blank=True, defult=timezone.now 
+        )
+        
